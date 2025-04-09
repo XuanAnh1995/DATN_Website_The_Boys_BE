8c7d7241339925bd693ac41d47b11614a0b8e55a
Lý Tính Nhiệm <136042948+rdagol@users.noreply.github.com>
2025-04-09 13:45:19 +0700

    Fix bug quantity of order online

--- src/main/java/backend/datn/services/OrderOnlineService.java
+++ src/main/java/backend/datn/services/OrderOnlineService.java
@@ -123,9 +123,6 @@ public class OrderOnlineService {
             detail.setPrice(salePrice);
             detail.setOrder(order);
 
-            productDetail.setQuantity(productDetail.getQuantity() - detailRequest.getQuantity());
-            productDetailRepository.save(productDetail);
-
             return detail;
         }).collect(Collectors.toList());
 
