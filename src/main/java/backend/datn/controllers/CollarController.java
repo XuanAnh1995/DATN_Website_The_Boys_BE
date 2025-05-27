717ab80916d7ef1c44b8b21587c62a4adb6c2a86
Lưu Việt Xuân Ánh <luuvietxuananh95@gmail.com>
2025-05-27 21:44:23 +0700

    Update_BE_Controller
    size = 100

--- src/main/java/backend/datn/controllers/CollarController.java
+++ src/main/java/backend/datn/controllers/CollarController.java
@@ -54,7 +54,7 @@ public class CollarController {
     public ResponseEntity<ApiResponse> getAllCollars(
             @RequestParam(required = false) String search,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "100") int size,
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(defaultValue = "asc") String sortDir) {
         try {
